5d871f2221e138de43837494780abfdcda2f3322
alvarotellez <alvarotellezhidalgo@gmail.com>
2017-01-25 10:36:06 +0100

    Codificar menu para configuración y contacto

--- app/src/main/java/es/iesnervion/atellez/proyectosemifinal/Activity_emergencia.java
+++ app/src/main/java/es/iesnervion/atellez/proyectosemifinal/Activity_emergencia.java
@@ -15,6 +15,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -76,6 +78,29 @@ public class Activity_emergencia extends AppCompatActivity implements View.OnCli
         } else {
             Log.i("Mensaje", "Provider is null");
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/es/iesnervion/atellez/proyectosemifinal/MainActivity.java
+++ app/src/main/java/es/iesnervion/atellez/proyectosemifinal/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         btnEnviar = (Button) findViewById(R.id.btnEntrar);
 
         editTextNombre = (EditText) findViewById(R.id.nomUsuario);
