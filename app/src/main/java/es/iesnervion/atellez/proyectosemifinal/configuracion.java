b6235e7bc39a78a702b2f5b46a2c95496a0f3743
alvarotellez <alvarotellezhidalgo@gmail.com>
2017-01-25 12:44:00 +0100

    Falta boton de volver atras y funcionalidad

--- app/src/main/java/es/iesnervion/atellez/proyectosemifinal/configuracion.java
+++ app/src/main/java/es/iesnervion/atellez/proyectosemifinal/configuracion.java
@@ -1,13 +1,24 @@
 package es.iesnervion.atellez.proyectosemifinal;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class configuracion extends AppCompatActivity {
-
+    private ImageView logo_config;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+        //Hacer que rote la imagen en cuanto entre el usuario en esa pestaña
+        logo_config = (ImageView) findViewById(R.id.img_config);
+        Animation movimiento;
+        movimiento = AnimationUtils.loadAnimation(this, R.anim.girar);
+        movimiento.reset();
+        logo_config.startAnimation(movimiento);
     }
 }
