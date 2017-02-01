b3d537fee2bfb62380919d966ef01d0bd93618ae
alvarotellez <alvarotellezhidalgo@gmail.com>
2017-02-01 10:46:35 +0100

    Limpio

--- app/src/main/java/es/iesnervion/atellez/proyectosemifinal/contacto.java
+++ app/src/main/java/es/iesnervion/atellez/proyectosemifinal/contacto.java
@@ -2,7 +2,7 @@ package es.iesnervion.atellez.proyectosemifinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Clase del layout Activity_contacto
 public class contacto extends AppCompatActivity {
 
     @Override
